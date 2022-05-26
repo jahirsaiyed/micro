5dd1087cd3a4dab8d108c48d421a7e00a9821ba4
JJS <jjsaiyed@gmail.com>
2022-05-26 06:29:21 +0400

    Added interfaces for all service classes

--- src/main/java/com/finance/investment/micro/service/InvestorService.java
+++ src/main/java/com/finance/investment/micro/service/InvestorService.java
@@ -1,80 +1,37 @@
 package com.finance.investment.micro.service;
 
-import com.finance.investment.micro.domain.Investor;
-import com.finance.investment.micro.repository.InvestorRepository;
 import com.finance.investment.micro.service.dto.InvestorDTO;
-import com.finance.investment.micro.service.mapper.InvestorMapper;
 import java.util.Optional;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 /**
- * Service Implementation for managing {@link Investor}.
+ * Service Interface for managing {@link com.finance.investment.micro.domain.Investor}.
  */
-@Service
-@Transactional
-public class InvestorService {
-
-    private final Logger log = LoggerFactory.getLogger(InvestorService.class);
-
-    private final InvestorRepository investorRepository;
-
-    private final InvestorMapper investorMapper;
-
-    public InvestorService(InvestorRepository investorRepository, InvestorMapper investorMapper) {
-        this.investorRepository = investorRepository;
-        this.investorMapper = investorMapper;
-    }
-
+public interface InvestorService {
     /**
      * Save a investor.
      *
      * @param investorDTO the entity to save.
      * @return the persisted entity.
      */
-    public InvestorDTO save(InvestorDTO investorDTO) {
-        log.debug("Request to save Investor : {}", investorDTO);
-        Investor investor = investorMapper.toEntity(investorDTO);
-        investor = investorRepository.save(investor);
-        return investorMapper.toDto(investor);
-    }
+    InvestorDTO save(InvestorDTO investorDTO);
 
     /**
-     * Update a investor.
+     * Updates a investor.
      *
-     * @param investorDTO the entity to save.
+     * @param investorDTO the entity to update.
      * @return the persisted entity.
      */
-    public InvestorDTO update(InvestorDTO investorDTO) {
-        log.debug("Request to save Investor : {}", investorDTO);
-        Investor investor = investorMapper.toEntity(investorDTO);
-        investor = investorRepository.save(investor);
-        return investorMapper.toDto(investor);
-    }
+    InvestorDTO update(InvestorDTO investorDTO);
 
     /**
-     * Partially update a investor.
+     * Partially updates a investor.
      *
      * @param investorDTO the entity to update partially.
      * @return the persisted entity.
      */
-    public Optional<InvestorDTO> partialUpdate(InvestorDTO investorDTO) {
-        log.debug("Request to partially update Investor : {}", investorDTO);
-
-        return investorRepository
-            .findById(investorDTO.getId())
-            .map(existingInvestor -> {
-                investorMapper.partialUpdate(existingInvestor, investorDTO);
-
-                return existingInvestor;
-            })
-            .map(investorRepository::save)
-            .map(investorMapper::toDto);
-    }
+    Optional<InvestorDTO> partialUpdate(InvestorDTO investorDTO);
 
     /**
      * Get all the investors.
@@ -82,31 +39,20 @@ public class InvestorService {
      * @param pageable the pagination information.
      * @return the list of entities.
      */
-    @Transactional(readOnly = true)
-    public Page<InvestorDTO> findAll(Pageable pageable) {
-        log.debug("Request to get all Investors");
-        return investorRepository.findAll(pageable).map(investorMapper::toDto);
-    }
+    Page<InvestorDTO> findAll(Pageable pageable);
 
     /**
-     * Get one investor by id.
+     * Get the "id" investor.
      *
      * @param id the id of the entity.
      * @return the entity.
      */
-    @Transactional(readOnly = true)
-    public Optional<InvestorDTO> findOne(Long id) {
-        log.debug("Request to get Investor : {}", id);
-        return investorRepository.findById(id).map(investorMapper::toDto);
-    }
+    Optional<InvestorDTO> findOne(Long id);
 
     /**
-     * Delete the investor by id.
+     * Delete the "id" investor.
      *
      * @param id the id of the entity.
      */
-    public void delete(Long id) {
-        log.debug("Request to delete Investor : {}", id);
-        investorRepository.deleteById(id);
-    }
+    void delete(Long id);
 }
